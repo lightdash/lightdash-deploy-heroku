b60b8208da3bcebc5908137b946190e2be393fc1
Oliver Laslett <olaslett@gmail.com>
2021-08-03 08:23:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
+# expect project as heroku config
 export LIGHTDASH_PROJECT_0_ACCOUNT_ID=$DBT_CLOUD_ACCOUNT_ID
 export LIGHTDASH_PROJECT_0_PROJECT_ID=$DBT_CLOUD_PROJECT_ID
 export LIGHTDASH_PROJECT_0_ENVIRONMENT_ID=$DBT_CLOUD_ENVIRONMENT_ID
 export LIGHTDASH_PROJECT_0_API_KEY=$DBT_CLOUD_API_KEY
 
+# heroku ssl fix
 export PGCONNECTIONURI=$DATABASE_URL?sslmode=no-verify
+
+# migrate db
+yarn workspace backend migrate-production
 
 exec "$@"
