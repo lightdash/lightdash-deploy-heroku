8f59ad9c5ac12fc4d68b7f09a9cf2248c3de5660
Jose Rego <jose@lightdash.com>
2022-04-29 10:02:25 +0100

    chore: enable debug logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 # Analytics
 export LIGHTDASH_INSTALL_TYPE=heroku
 
+# Enable debug logs
+export LIGHTDASH_LOG_LEVEL=debug
+
 # heroku ssl fix
 export PGCONNECTIONURI=$DATABASE_URL?sslmode=no-verify
 
